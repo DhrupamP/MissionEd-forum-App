d1216762d209d1221b0c730a1bae0b8536a90f12
DhrupamP <63700466+DhrupamP@users.noreply.github.com>
2021-07-30 09:59:39 +0400

    worked out navbuttons

--- lib/screens/network_page.dart
+++ lib/screens/network_page.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:missioned_forum/components/my_app_bar.dart';
 import 'package:missioned_forum/components/my_drawer.dart';
 
+var btnindex = 0;
+bool pressed = false;
+
 class NetworkPage extends StatefulWidget {
   const NetworkPage({Key? key}) : super(key: key);
 
@@ -12,56 +15,101 @@ class NetworkPage extends StatefulWidget {
 class _NetworkPageState extends State<NetworkPage> {
   bool pressed = false;
 
+  List<Widget> bodyElements = [
+    Text('No One is Followinf you!'),
+    Text('your are not following anyone!'),
+    Container(
+      color: Colors.red,
+      height: 100,
+      width: double.infinity,
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(),
       endDrawer: MyDrawer(),
-      body: Container(
-        padding: EdgeInsets.only(top: 40, left: 20, right: 20),
-        child: Align(
-          alignment: Alignment.topCenter,
-          child: Material(
-              borderRadius: BorderRadius.circular(10),
-              elevation: 10,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Followers',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                  Container(
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(
-                            pressed ? Colors.black : Colors.white),
-                      ),
-                      onPressed: () {
-                        setState(() => pressed = !pressed);
-                      },
-                      child: Container(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          'Following',
-                          style: TextStyle(fontWeight: FontWeight.bold),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 30, horizontal: 15),
+            child: Align(
+              alignment: Alignment.topCenter,
+              child: Material(
+                  borderRadius: BorderRadius.circular(10),
+                  elevation: 10,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  pressed ? Colors.black : Colors.black)),
+                          onPressed: () {
+                            setState(() {
+                              pressed = !pressed;
+                              btnindex = 0;
+                            });
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Followers',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Recommended',
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ],
-              )),
-        ),
+                      Container(
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  pressed ? Colors.black : Colors.black)),
+                          onPressed: () {
+                            setState(() {
+                              pressed = !pressed;
+                              btnindex = 1;
+                            });
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Following',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(
+                                  pressed ? Colors.black : Colors.black)),
+                          onPressed: () {
+                            setState(() {
+                              pressed = !pressed;
+                              btnindex = 2;
+                            });
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Recommended',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  )),
+            ),
+          ),
+          Container(
+            child: bodyElements.elementAt(btnindex),
+          )
+        ],
       ),
     );
   }
